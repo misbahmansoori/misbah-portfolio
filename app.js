bba2122d87e8e292c81ab47d150918211658db75
Misbah Mansoori <misbahmansoori14@gmail.com>
2025-07-09 14:51:11 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("open");
+  });
+
+  
